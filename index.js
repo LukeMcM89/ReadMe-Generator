548257b79ecc7bf4c076444180478b27e158b973
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-07-29 11:52:41 -0400

    Question Array initials

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const fs = require('inquirer');
 const fs = require('fs');
 const util = require("util");
-
-
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-`<!DOCTYPE html>
-<html lang="en>
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="ie-edge">
+const questions = [ {
+    type: "input",
+    queston: "What would you like to name your project?"
+    name: "name"
+},
+    {
+        type: "input",
+        question: ""
+        name: ""
+    }]
 
 
-
-
-
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

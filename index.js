014725ad49fd00db28894981d382c9300530e493
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-07-29 12:42:18 -0400

    More Questions

--- index.js
+++ index.js
@@ -5,13 +5,30 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 const questions = [ {
     type: "input",
-    question: "What would you like to name your project?"
+    question: "What would you like to name your project?",
     name: "name"
 },
     {
         type: "input",
-        question: ""
-        name: ""
+        question: "Enter a description of your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        question:"What are the installation requirements?",
+        name: "install"
+    },
+    {
+        type: 'input',
+        question: "When is the project run?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        question: "Select and then Enter the Open-Source license variant for your project.",
+        name: "license",
+        choices: [MIT, Apache 2.0, Mozilla Public License2.0,]
+
     }]
 
 

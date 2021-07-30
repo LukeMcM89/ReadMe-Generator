5c5e8a9277398269e7a2309c3a7e4731b4d95b53
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-07-30 15:13:53 -0400

    Question prompt debugs

--- index.js
+++ index.js
@@ -3,45 +3,55 @@ const fs = require('fs');
 const util = require("util");
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-const questions = [ {
+const questions = [{
     type: "input",
     question: "What would you like to name your project?",
-    name: "name"
+    name: "Name"
 },
-    {
-        type: "input",
-        question: "Enter a description of your project.",
-        name: "description"
+{
+    type: "input",
+    question: "Enter a description of your project.",
+    name: "Description"
+},
+{
+    type: "input",
+    question: "What are the installation requirements?",
+    name: "Install"
+},
+{
+    type: "input",
+    question: "When is the project run?",
+    name: "Usage"
+},
+{
+    type: "list",
+    question: "Select and then Enter the Open-Source license variant for your project.",
+    name: "License",
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'Mozilla Public License2.0'],
     },
-    {
-        type: "input",
-        question:"What are the installation requirements?",
-        name: "install"
-    },
-    {
-        type: "input",
-        question: "When is the project run?",
-        name: "usage"
-    },
-    {
-        type: "list",
-        question: "Select and then Enter the Open-Source license variant for your project.",
-        name: "license",
-        choices: ["MIT,' "Apache 2.0," 'Mozilla Public License2.0'],
-    },
-    {
-        type: "input",
-        question: "If this is a group project, what other individuals contributed?",
-        name: "contributions"
-    }]
-
+{
+    type: "input",
+    question: "If this is a group project, what other individuals contributed?",
+    name: "Contributors"
+},
+{
+    type: "input",
+    question: "Enter an email address.",
+    name: "Email"
+},
+{
+    type: "input",
+    question: "Enter your GitHub username.",
+    name: "GitHub"
+}
+]
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
